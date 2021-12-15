70c28ab639d895297d6a4547a80bb758335dc514
lr manojkumar <lrmanoj33@gmail.com>
2021-12-15 21:29:29 +0530

    Base commit

---
+++ src/gitbash/Git.java
@@ -0,0 +1,17 @@
+package gitbash;
+
+public class Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+          int a=10;
+          int b=30;
+          if (a>b) {
+          System.out.println("true");
+          
+          }else {
+        	  System.out.println("false");
+        		 
+	}}
+
+}
